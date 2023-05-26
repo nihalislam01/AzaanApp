152199bd3d0996c193aa27a8948dc7593f014431
Nihal <nihalislam2@gmail.com>
2023-05-26 21:01:51 +0600

    Initial Commit

---
+++ Azaan/AzaanApp.swift
@@ -0,0 +1,17 @@
+//
+//  AzaanApp.swift
+//  Azaan
+//
+//  Created by Nihal Islam on 26/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AzaanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
